a8f12109d11191268d084e40fcd3b26a6abcdd38
abhishek <abhishekshrivastav1920@gmail.com>
2023-12-08 19:46:11 +0530

    boilerplate server setup

---
+++ server/connection.js
@@ -0,0 +1,19 @@
+const  mongoose  = require('mongoose');
+const connectToMongoDB = async () => {
+    try {
+        
+        const response = await mongoose.connect(process.env.MONGODB_URL, {
+            dbName : process.env.JOBPORTAL
+        })
+        if (response) {
+            console.log("connected successfully to mongodb");
+        }
+    } catch (e) {
+        console.error("something went wrong", e.message);
+    }
+}
+
+
+module.exports = {
+    connectToMongoDB,
+}
--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 
-
+const dotenv = require('dotenv');
 const exprees = require('express');
+const { connectToMongoDB } = require('./connection');
 
 const app = exprees();
 
+dotenv.config();
 
+app.get('/healthz', (req, res) => {
+     
+    res.send({
+        message: "working fine",
+        health: "ok",
+        time: Date.now()
+    })
+})
+app.listen(process.env.PORT, () => {
 
-app.listen(3000, () => {
-    console.log("hello world");
+    connectToMongoDB();
+    console.log("working on port ",process.env.PORT);
 })
